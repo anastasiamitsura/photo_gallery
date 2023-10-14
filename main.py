8e6c446299f59de2db556a254797a76844d0dd8b
Stacey <nastamicura2@gmail.com>
2023-10-14 22:28:11 +0700

    Camera open v2

--- main.py
+++ main.py
@@ -1,86 +1,102 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtMultimediaWidgets import *
+import os
 import sys
-import cv2
-import numpy as np
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.Qt import *
+import time
 
 
-class Ui_Widget(object):
-    def setupUi(self, Widget):
-        Widget.setObjectName("Widget")
-        Widget.resize(943, 465)
-
-        self.simulation = QtWidgets.QPushButton(Widget)
-        self.simulation.setGeometry(QtCore.QRect(10, 440, 80, 21))
-        self.simulation.setObjectName("simulation")
-        self.simulation_end = QtWidgets.QPushButton(Widget)
-        self.simulation_end.setGeometry(QtCore.QRect(100, 440, 80, 21))
-        self.simulation_end.setObjectName("simulation_end")
-        self.label_video = QLabel(Widget)
-        self.label_video.setGeometry(QtCore.QRect(290, 20, 311, 201))
-        self.label_video.setObjectName("label_video")
-
-        self.retranslateUi(Widget)
-        QtCore.QMetaObject.connectSlotsByName(Widget)
-
-    def retranslateUi(self, Widget):
-        _translate = QtCore.QCoreApplication.translate
-        Widget.setWindowTitle(_translate("Widget", "Widget"))
-        self.simulation.setText(_translate("Widget", "открыть камеру"))
-        self.simulation_end.setText(_translate("Widget", "закрыть камеру"))
-
-class ThreadOpenCV(QThread):
-    changePixmap = pyqtSignal(QImage)
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
-    def run(self):
-        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        cap.set(cv2.CAP_PROP_FPS, 24)
+        self.setGeometry(100, 100,
+                         800, 600)
+        self.setStyleSheet("background : lightgrey;")
+        self.available_cameras = QCameraInfo.availableCameras()
+        if not self.available_cameras:
+            sys.exit()
 
-        while True:
-            ret, frame = cap.read()
-            if ret:
-                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                frame_expanded = np.expand_dims(frame_rgb, axis=0)
-                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                h, w, ch = rgbImage.shape
-                bytesPerLine = ch * w
-                convertToQtFormat = QImage(
-                    rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
-                p = convertToQtFormat.scaled(311, 201, Qt.KeepAspectRatio)
-                self.changePixmap.emit(p)
-
-                if cv2.waitKey(1) == ord('q'):
-                    break
-
-            self.msleep(20)
-            cv2.destroyAllWindows()
-
-class Window(QtWidgets.QWidget, Ui_Widget):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-        self.simulation.clicked.connect(self.can)
-        self.thread = ThreadOpenCV()
-        self.thread.changePixmap.connect(self.setImage)
+        self.status = QStatusBar()
+        self.status.setStyleSheet("background : white;")
+        self.setStatusBar(self.status)
+        self.save_path = ""
+        self.viewfinder = QCameraViewfinder()
+        self.viewfinder.show()
+        self.setCentralWidget(self.viewfinder)
+        self.select_camera(0)
+        toolbar = QToolBar("Camera Tool Bar")
+        self.addToolBar(toolbar)
+        click_action = QAction("Click photo", self)
+        click_action.setStatusTip("This will capture picture")
+        click_action.setToolTip("Capture picture")
+        click_action.triggered.connect(self.click_photo)
+        toolbar.addAction(click_action)
+        change_folder_action = QAction("Change save location",
+                                       self)
+        change_folder_action.setStatusTip("Change folder where picture will be saved saved.")
+        change_folder_action.setToolTip("Change save location")
+        change_folder_action.triggered.connect(self.change_folder)
+        toolbar.addAction(change_folder_action)
+        camera_selector = QComboBox()
+        camera_selector.setStatusTip("Choose camera to take pictures")
+        camera_selector.setToolTip("Select Camera")
+        camera_selector.setToolTipDuration(2500)
+        camera_selector.addItems([camera.description()
+                                  for camera in self.available_cameras])
+        camera_selector.currentIndexChanged.connect(self.select_camera)
+        toolbar.addWidget(camera_selector)
+        toolbar.setStyleSheet("background : white;")
+        self.setWindowTitle("PyQt5 Cam")
 
         self.show()
 
-    def can(self):
-        self.thread.start()
+    def select_camera(self, i):
+        self.camera = QCamera(self.available_cameras[i])
+        self.camera.setViewfinder(self.viewfinder)
+        self.camera.setCaptureMode(QCamera.CaptureStillImage)
+        self.camera.error.connect(lambda: self.alert(self.camera.errorString()))
+        self.camera.start()
+        self.capture = QCameraImageCapture(self.camera)
+        self.capture.error.connect(lambda error_msg, error,
+                                          msg: self.alert(msg))
+        self.capture.imageCaptured.connect(lambda d,
+                                                  i: self.status.showMessage("Image captured : "
+                                                                             + str(self.save_seq)))
+        self.current_camera_name = self.available_cameras[i].description()
+        self.save_seq = 0
 
 
-    def setImage(self, image):
-        self.label_video.setPixmap(QPixmap.fromImage(image))
+    def click_photo(self):
+
+        timestamp = time.strftime("%d-%b-%Y-%H_%M_%S")
+
+        self.capture.capture(os.path.join(self.save_path,
+                                          "%s-%04d-%s.jpg" % (
+                                              self.current_camera_name,
+                                              self.save_seq,
+                                              timestamp
+                                          )))
+
+        self.save_seq += 1
+
+
+    def change_folder(self):
+        path = QFileDialog.getExistingDirectory(self,
+                                                "Picture Location", "")
+        if path:
+            self.save_path = path
+
+            self.save_seq = 0
+
+    def alert(self, msg):
+        error = QErrorMessage(self)
+        error.showMessage(msg)
+
 
 
 if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    w = Window()
-    w.show()
-    sys.exit(app.exec_())
+    App = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(App.exec())
